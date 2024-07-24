fd40bb8df0d2a978076192bb4aff9a18acbc6e46
saralotfi <saralotfi3442@gmail.com>
2024-07-24 10:19:17 +0330

    Refactored using class

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,94 +2,96 @@
 #include <windows.h> 
 using namespace std;
 
-char board[3][3];  
-
-void initializeBoard() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            board[i][j] = '-';
-        }
-    }
-}
-
-void draw() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << board[i][j] << ' ';
-        }
-        cout << endl;
-    }
-}
-
-bool isFull() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[i][j] == '-')
-                return false;
-        }
-    }
-    return true;
-}
-
-bool isWin(char player) {
-
-    for (int i = 0; i < 3; i++) {
-        if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
-            return true;
+class TicTacToe {
+public:
+    TicTacToe() {
+        initializeBoard();
     }
 
-    for (int j = 0; j < 3; j++) {
-        if (board[0][j] == player && board[1][j] == player && board[2][j] == player)
-            return true;
-    }
+    void play() {
+        char currentPlayer = 'X';
+        int row, col;
 
-    if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
-        return true;
-    if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
-        return true;
-    
-    return false;
-}
-
-void  playTurn() {
-    char currentPlayer = 'X';
-    int row, col;
-    
-    initializeBoard();
-    
-    while (!isFull()) {
-        draw();
-        cout << "Player " << currentPlayer << " row? (0-2): ";
-        cin >> row;
-        cout << "Col? (0-2): ";
-        cin >> col;
-
-
-        if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != '-') {
-            cout << "Invalid input, please re-enter." << endl;
-            continue;
-        }
-
-
-        board[row][col] = currentPlayer;
-
-
-        if (isWin(currentPlayer)) {
+        while (!isFull()) {
             draw();
-            cout << "Player " << currentPlayer << " wins!" << endl;
-            return;
+            cout << "Player " << currentPlayer << ", enter row (0-2): ";
+            cin >> row;
+            cout << "Enter col (0-2): ";
+            cin >> col;
+
+            if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != '-') {
+                cout << "Invalid input, please re-enter." << endl;
+                continue;
+            }
+
+            board[row][col] = currentPlayer;
+            if (isWin(currentPlayer)) {
+                draw();
+                cout << "Player " << currentPlayer << " wins!" << endl;
+                return;
+            }
+
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+
+            system("cls"); 
         }
-        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
 
+        draw();
+        cout << "It's a draw!" << endl;
     }
-    system("cls"); 
-    draw();
-    cout << "It's a draw!" << endl;
 
-}
+private:
+    char board[3][3]; 
 
+    void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    void draw() {
+        cout << "Current board:" << endl;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cout << board[i][j] << ' ';
+            }
+            cout << endl;
+        }
+    }
+
+    bool isFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-')
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    bool isWin(char player) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
+                return true;
+        }
+
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == player && board[1][j] == player && board[2][j] == player)
+                return true;
+        }
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
+            return true;
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
+            return true;
+
+        return false;
+    }
+};
 
 int main() {
-    playTurn();
+    TicTacToe game;
+    game.play();
     return 0;
 }
