215cf8effadb43f2f43ad5915bb28cd1f56fcc3e
saralotfi <saralotfi3442@gmail.com>
2024-07-24 21:17:18 +0330

    grind function new function

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <windows.h> 
+#include <windows.h>
 using namespace std;
 
 class TicTacToe {
@@ -8,32 +8,23 @@ public:
         initializeBoard();
     }
 
-    void play() {
-        char currentPlayer = 'X';
-        int row, col;
+    void playTurn() {
+        currentPlayer = 'X';
+        while (!isBoardFull()) {
+            draw(); 
 
-        while (!isFull()) {
-            draw();
-            cout << "Player " << currentPlayer << ", enter row (0-2): ";
-            cin >> row;
-            cout << "Enter col (0-2): ";
-            cin >> col;
-
-            if (row < 0 || row > 2 || col < 0 || col > 2 || board[row][col] != '-') {
-                cout << "Invalid input, please re-enter." << endl;
+            if (!takePlayerTurn()) {
                 continue;
             }
 
-            board[row][col] = currentPlayer;
             if (isWin(currentPlayer)) {
                 draw();
-                cout << "Player " << currentPlayer << " wins!" << endl;
-                return;
+                cout << currentPlayer << " is win!" << endl;
+                return; 
             }
 
-            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-
-            system("cls"); 
+            clearScreen();
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X'; 
         }
 
         draw();
@@ -41,57 +32,123 @@ public:
     }
 
 private:
-    char board[3][3]; 
+    char board[3][3];
+    char currentPlayer;
 
     void initializeBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                board[i][j] = '-'; 
             }
         }
     }
 
     void draw() {
-        cout << "Current board:" << endl;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
                 cout << board[i][j] << ' ';
             }
             cout << endl;
         }
+        cout << endl;
     }
 
-    bool isFull() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == '-')
-                    return false;
+    bool isBoardFull() {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                if (board[i][j] == '-') {
+                    return false; 
+                }
+            }
+        }
+        return true; 
+    }
+
+    bool allEqual(const char arr[], char player) {
+        for (int i = 0; i < 3; ++i) {
+            if (arr[i] != player) {
+                return false;
             }
         }
         return true;
     }
 
-    bool isWin(char player) {
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
-                return true;
+    void getColumn(int col, char arr[]) {
+        for (int i = 0; i < 3; ++i) {
+            arr[i] = board[i][col];
         }
+    }
 
-        for (int j = 0; j < 3; j++) {
-            if (board[0][j] == player && board[1][j] == player && board[2][j] == player)
+    bool checkRows(char player) {
+        for (int i = 0; i < 3; ++i) {
+            if (allEqual(board[i], player)) {
                 return true;
+            }
         }
-        if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
-            return true;
-        if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
-            return true;
-
         return false;
+    }
+
+    bool checkColumns(char player) {
+        for (int col = 0; col < 3; ++col) {
+            char column[3];
+            getColumn(col, column);
+            if (allEqual(column, player)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool checkDiagonals(char player) {
+        char mainDiagonal[3];
+        char antiDiagonal[3];
+
+        for (int i = 0; i < 3; ++i) {
+            mainDiagonal[i] = board[i][i];
+            antiDiagonal[i] = board[i][2 - i];
+        }
+
+        return allEqual(mainDiagonal, player) || allEqual(antiDiagonal, player);
+    }
+
+    bool isWin(char player) {
+        return checkRows(player) || checkColumns(player) || checkDiagonals(player);
+    }
+
+    bool takePlayerTurn() {
+        int row, col;
+        cout << currentPlayer << ", row? (0 , 2): ";
+        cin >> row;
+        cout << "col? (0 , 2): ";
+        cin >> col;
+
+        if (!cin) { 
+            cin.clear(); 
+            cout << "Enter valid numbers." << endl;
+            return false;
+        }
+
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-') {
+            board[row][col] = currentPlayer;
+            return true;
+        } else {
+            cout << "Invalid input, please re-enter." << endl;
+            return false; 
+        }
+    }
+
+    void clearScreen() {
+            system("cls");
+
     }
 };
 
 int main() {
     TicTacToe game;
-    game.play();
+    game.playTurn();
     return 0;
 }
+
+
+
+
