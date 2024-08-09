2462bb1348e14a064c89397cb9dfb9a90abc0436
saralotfi <saralotfi3442@gmail.com>
2024-08-09 22:20:43 +0330

    fix drawBoard

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -19,75 +19,83 @@ public:
         currentPlayer = 'X';
         int row = 0, col = 0;
         bool gameWon = false;
-        while (!isBoardFull& !gameWon()) {
-            clearScreen();
-            draw(row, col,gameWon); 
 
-            if (!PlayerTurn(row,col)) {
+        while (!isBoardFull() && !gameWon) {
+            clearScreen();
+            drawBoard(row, col, gameWon);
+
+            if (!PlayerTurn(row, col)) {
                 continue;
             }
 
             if (isWin(currentPlayer)) {
                 gameWon = true;
                 clearScreen();
-                draw(row, col);
-                cout << currentPlayer << " is win!" << endl;
-                
+                drawBoard(row, col, gameWon);
+                cout << currentPlayer << " wins!" << endl;
                 return; 
             }
 
             currentPlayer = (currentPlayer == 'X') ? 'O' : 'X'; 
         }
 
-        clearScreen();
-        draw(row, col,gameWon);
-        cout << "equal" << endl;
-    }
-
-    if (!gameWon) {
-        clearScreen();
-        drawBoard(row, col, gameWon);
-        cout << "equal" << endl;
+        if (!gameWon) {
+            clearScreen();
+            drawBoard(row, col, gameWon);
+            cout << "It's a draw!" << endl;
         }
-}
+    }
 
 private:
     char board[3][3];
     char currentPlayer;
+    int winCells[3][2];
 
     void initializeBoard() {
-        for (int i = 0; i < 3;i++) {
-            for (int j = 0; j < 3;j++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 board[i][j] = '-'; 
             }
         }
-    }
-
-     void drawCell(int i, int j) {
-        cout << ' ' << board[i][j] << ' ';
     }
 
     void drawBoard(int cursorRow, int cursorCol, bool gameWon) {
         for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 3; ++j) {
                 setColorForCell(i, j, cursorRow, cursorCol, gameWon);
-                drawCell(i, j);
+                if (i == cursorRow && j == cursorCol) {
+                    cout << '[' << currentPlayer << "] ";
+                } else {
+                    drawCell(i, j);
+                }
             }
             cout << endl;
         }
-        void setColorForCell(int i, int j, int cursorRow, int cursorCol, bool gameWon) {
-        if (gameWon && isWinCell(i, j)) {
-            SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-        } else if (gameWon && board[i][j] != '-' && !isWinCell(i, j)) {ه
-            SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
-        } else  (i == cursorRow && j == cursorCol) {
-            SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-        } 
-    }
 
         resetColor();
         cout << endl;
     }
+
+    void setColorForCell(int i, int j, int cursorRow, int cursorCol, bool gameWon) {
+        if (gameWon && isWinCell(i, j)) {
+            SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+        } else if (gameWon && board[i][j] != '-' && !isWinCell(i, j)) {
+            SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+        } else if (i == cursorRow && j == cursorCol) {
+            SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+        } else {
+            SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+        }
+    }
+
+    void drawCell(int i, int j) {
+        cout << ' ' << board[i][j] << ' ';
+    }
+
+    void resetColor() {
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    }
+
     bool isBoardFull() {
         for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 3; ++j) {
@@ -117,6 +125,10 @@ private:
     bool checkRows(char player) {
         for (int i = 0; i < 3; ++i) {
             if (allEqual(board[i], player)) {
+                for (int j = 0; j < 3; ++j) {
+                    winCells[j][0] = i;
+                    winCells[j][1] = j;
+                }
                 return true;
             }
         }
@@ -128,6 +140,10 @@ private:
             char column[3];
             getColumn(col, column);
             if (allEqual(column, player)) {
+                for (int i = 0; i < 3; ++i) {
+                    winCells[i][0] = i;
+                    winCells[i][1] = col;
+                }
                 return true;
             }
         }
@@ -143,12 +159,38 @@ private:
             antiDiagonal[i] = board[i][2 - i];
         }
 
-        return allEqual(mainDiagonal, player) || allEqual(antiDiagonal, player);
+        if (allEqual(mainDiagonal, player)) {
+            for (int i = 0; i < 3; ++i) {
+                winCells[i][0] = i;
+                winCells[i][1] = i;
+            }
+            return true;
+        }
+
+        if (allEqual(antiDiagonal, player)) {
+            for (int i = 0; i < 3; ++i) {
+                winCells[i][0] = i;
+                winCells[i][1] = 2 - i;
+            }
+            return true;
+        }
+
+        return false;
     }
 
     bool isWin(char player) {
         return checkRows(player) || checkColumns(player) || checkDiagonals(player);
     }
+    
+    bool isWinCell(int row, int col) {
+        for (int i = 0; i < 3; ++i) {
+            if (winCells[i][0] == row && winCells[i][1] == col) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     void moveCursorRow(int &row, int direction) {
         if (direction == -1) {
             row = (row > 0) ? row - 1 : 2; 
@@ -156,67 +198,71 @@ private:
             row = (row < 2) ? row + 1 : 0; 
         }
     }
-     void moveCursorCol(int &col, int direction) {
+
+    void moveCursorCol(int &col, int direction) {
         if (direction == -1) {
             col = (col > 0) ? col - 1 : 2; 
         } else {
             col = (col < 2) ? col + 1 : 0;
         }
     }
+
     bool PlayerTurn(int& row, int& col) {
-    while (true) {
-        if (_kbhit()) { 
-            int key = _getch();
-            cout << "Key pressed: " << key << endl;
-            if (key == 224) {
-                key = _getch();
-                cout << "Arrow key detected: " << key << endl;
-                switch (key) {
-                    case UP_ARROW:
-                        moveCursorRow(row, -1);
-                        break;
-                    case DOWN_ARROW:
-                        moveCursorRow(row, 1);
-                        break;
-                    case LEFT_ARROW:
-                        moveCursorCol(col, -1);
-                        break;
-                    case RIGHT_ARROW:
-                        moveCursorCol(col, 1);
-                        break;
+        while (true) {
+            if (_kbhit()) { 
+                int key = _getch();
+                cout << "Key pressed: " << key << endl;
+                if (key == 224) {
+                    key = _getch();
+                    cout << "Arrow key detected: " << key << endl;
+                    switch (key) {
+                        case UP_ARROW:
+                            moveCursorRow(row, -1);
+                            break;
+                        case DOWN_ARROW:
+                            moveCursorRow(row, 1);
+                            break;
+                        case LEFT_ARROW:
+                            moveCursorCol(col, -1);
+                            break;
+                        case RIGHT_ARROW:
+                            moveCursorCol(col, 1);
+                            break;
+                    }
+                } else if (key == 13) { 
+                    cout << "Enter key pressed. Checking move validity..." << endl;
+                    if (isValidMove(row, col)) {
+                        board[row][col] = currentPlayer;
+                        cout << "Move made at (" << row << ", " << col << ") by " << currentPlayer << endl;
+                        return true;
+                    } else {
+                        cout << "Invalid move at (" << row << ", " << col << "). Please try again." << endl;
+                        return false;
+                    }
                 }
-            } else if (key == 13) { 
-                cout << "Enter key pressed. Checking move validity..." << endl;
-                if (isValidMove(row, col)) {
-                    board[row][col] = currentPlayer;
-                    cout << "Move made at (" << row << ", " << col << ") by " << currentPlayer << endl;
-                    return true;
-                } else {
-                    cout << "Invalid move at (" << row << ", " << col << "). Please try again." << endl;
-                    return false;
-                }
+                clearScreen();
+                drawBoard(row, col, false);
             }
-            clearScreen();
-            draw(row, col);
         }
     }
-}
 
-     bool isInSideTable(int row, int col) {
+    bool isInSideTable(int row, int col) {
         return (row >= 0 && row < 3 && col >= 0 && col < 3);
     }
-     bool isCellEmpty(int row, int col) {
+
+    bool isCellEmpty(int row, int col) {
         return (board[row][col] == '-');
-     }
+    }
     
-     bool isValidMove(int row, int col) {
+    bool isValidMove(int row, int col) {
         return isInSideTable(row, col) && isCellEmpty(row, col);
     }
 
     void clearScreen() {
         system("cls");
     }
- };
+};
+
 int main() {
     TicTacToe game;
     game.playGame();
